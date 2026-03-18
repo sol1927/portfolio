0f7bcd994ec93c800e281cd5871263b027d68577
Solomon <solomonaragaw691@gmail.com>
2026-03-18 19:50:18 +0300

    Add the logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { IoMenu } from "react-icons/io5";
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -49,10 +49,12 @@ const Navbar = () => {
         id="navbar"
         className="fixed top-0 left-0 w-full z-50 p-4 sm:p-6 flex justify-between items-center backdrop-blur-xs bg-black/10 shadow"
       >
-        <div className='flex items-center justify-center bg-blue-950 aspect-square w-16 sm:w-20 rounded-full shadow-[2px_10px_10px_2px] shadow-gray-700 hover:scale-110 hover:shadow-[0px_0px_15px_15px] hover:shadow-blue-500 hover:animate-bounce transition-all duration-800'>
-          <div className='text-cyan-400 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl'>
-            Sol
-          </div>
+        <div className='w-16 sm:w-20 aspect-square rounded-full overflow-hidden shadow-[2px_10px_10px_2px] shadow-gray-700 hover:scale-110 hover:shadow-blue-500 transition-all duration-300'>
+          <img
+            src="/SA.png"
+            alt="logo"
+            className="w-full h-full object-cover"
+          />
         </div>
 
         <nav className='hidden md:flex md:w-[70%] lg:w-[70%] justify-between items-center bg-gray-300 font-semibold rounded-2xl shadow-gray-600 px-10 py-4 max-w-2xl shadow-[2px_10px_10px_2px] hover:scale-105 hover:shadow-blue-500 transition-all duration-300'>
