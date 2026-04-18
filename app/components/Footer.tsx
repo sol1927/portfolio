c7bc933d3569e672fed71b0851059bb4d846d233
Solomon Aragaw <solomonaragaw691@gmail.com>
2026-04-18 13:00:01 +0300

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { LuGithub, LuYoutube } from "react-icons/lu";
 import { FiLinkedin, FiInstagram } from "react-icons/fi";
-import { SiUpwork } from "react-icons/si;
+import { SiUpwork } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const Footer = () => {
